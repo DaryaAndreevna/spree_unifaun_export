c47a11cc169d8f9985ffea4b96bdd541ca96bb09
Karazaeva Daria <karazayeva.darya@gmail.com>
2017-10-16 19:05:08 +0300

    Change csv output

--- app/overrides/admin_unifaun_export_sidebar.rb
+++ app/overrides/admin_unifaun_export_sidebar.rb
@@ -1,6 +1,6 @@
-Deface::Override.new(
-  virtual_path:  'spree/admin/orders/index',
-  name:          'add_unifaun_export_admin_configurations_menu',
-  insert_before: '#listing_orders',
-  text:          '<%= configurations_sidebar_menu_item Spree.t(:settings, scope: :unifaun_export), spree.edit_admin_unifaun_export_settings_path %>'
-)
+# Deface::Override.new(
+#   virtual_path:  'spree/admin/orders/index',
+#   name:          'add_unifaun_export_admin_configurations_menu',
+#   insert_before: '#listing_orders',
+#   text:          '<%= configurations_sidebar_menu_item Spree.t(:settings, scope: :unifaun_export), spree.edit_admin_unifaun_export_settings_path %>'
+# )
