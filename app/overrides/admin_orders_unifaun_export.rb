4994b4ea1f9ff211eb2e69385e2ffb22379e629b
Karazaeva Daria <karazayeva.darya@gmail.com>
2017-10-16 18:42:58 +0300

    Rewrite deface overrides to fit solidus

--- app/overrides/admin_orders_unifaun_export.rb
+++ app/overrides/admin_orders_unifaun_export.rb
@@ -1,10 +1,10 @@
 Deface::Override.new(
   :virtual_path => 'spree/admin/orders/index',
   :name => 'add_unifaun_export_button',
-  :insert_after => "erb[loud]:contains(':simple => true')",
+  :insert_before => "#listing_orders",
   :text => "
     <div class='form-group pull-right'>
-    <%= button_link_to 'Export for Unifaun', admin_orders_path(format: :csv, params: request.query_parameters), :class => 'btn-success', :id => 'admin_export_order' %>
+    <%= button_link_to 'Export', admin_orders_path(format: :csv, params: request.query_parameters), :class => 'btn-success', :id => 'admin_export_order' %>
     </div>
   "
 )
